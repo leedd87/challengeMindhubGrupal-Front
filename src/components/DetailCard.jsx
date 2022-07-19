f226df6d418c4a6eaac1ad21c42cfe86b83bed2f
tomasAybar <aybar.t4@gmail.com>
2022-07-19 18:02:57 -0300

    prueba de colores de bg

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -5,7 +5,9 @@ const DetailCard = () => {
     return (
         <>
             {/* contenedor de div imagenes y div botones de agregar carrito */}
-            <div className='sm:container sm:mx-auto grid grid-cols-5 p-2 sm:p-10 gap-5 border border-red-400'>
+            <div className='sm:container sm:mx-auto grid grid-cols-5 p-2 sm:p-10 gap-5 border border-red-400 bg-white rounded-md'
+            
+            >
 
                 {/* card productos y descripcion */}
                 <div className='col-span-5 md:col-span-3 flex justify-center items-center flex-col p-1'>
@@ -15,22 +17,22 @@ const DetailCard = () => {
 
                         <div className='col-span-4 lg:col-span-3'>
                             <img
-                                src='https://wave.fr/images/1916/08/nike-air-max-1-wmns-triple-white.jpg'
+                                src='https://img.hypeboost.com/products/new-balance-2002r-nightwatch-green/w900/img01.jpg'
                                 alt='producto'
                             />
                         </div>
 
                         <div className='hidden lg:block lg:col-span-1'>
                             <img
-                                src='https://wave.fr/images/1916/08/nike-air-max-1-wmns-triple-white.jpg'
+                                src='https://img.hypeboost.com/products/new-balance-2002r-nightwatch-green/w900/img01.jpg'
                                 alt='producto'
                             />
                             <img
-                                src='https://wave.fr/images/1916/08/nike-air-max-1-wmns-triple-white.jpg'
+                                src='https://img.hypeboost.com/products/new-balance-2002r-nightwatch-green/w900/img01.jpg'
                                 alt='producto'
                             />
                             <img
-                                src='https://wave.fr/images/1916/08/nike-air-max-1-wmns-triple-white.jpg'
+                                src='https://img.hypeboost.com/products/new-balance-2002r-nightwatch-green/w900/img01.jpg'
                                 alt='producto'
                             />
                         </div>
@@ -39,7 +41,7 @@ const DetailCard = () => {
 
                     {/* Descripcion */}
                     <div className='w-full h-10 md:h-20 flex items-center justify-center text-sm md:text-base border border-red-400'>
-                        <p className='font-bold uppercase'>Descripcion</p>
+                        <p className='uppercase font-bold'>Descripcion</p>
                     </div>
 
                 </div>
@@ -67,9 +69,9 @@ const DetailCard = () => {
                     {/* Botones */}
                     <div className=' flex items-center justify-center flex-col gap-5 py-4 md:py-8 md:text-sm'>
 
-                        <button class='bg-indigo-500 text-white font-bold px-1 lg:px-5 py-2 lg:py-2 text-sm lg:text-base rounded-md hover:bg-indigo-700 shadow-md w-2/5 sm:w-2/3'>Agregar al carrito</button>
+                        <button class='bg-green-600 text-white font-bold px-1 lg:px-5 py-2 lg:py-2 text-sm lg:text-base rounded-md hover:bg-indigo-700 shadow-md w-2/5 sm:w-2/3'>Agregar al carrito</button>
 
-                        <button class='bg-indigo-500 text-white font-bold px-1 lg:px-5 py-2 lg:py-2 text-sm lg:text-base rounded-md hover:bg-indigo-700 shadow-md w-2/5 sm:w-2/3'>Comprar ahora</button>
+                        <button class='bg-green-600 text-white font-bold px-1 lg:px-5 py-2 lg:py-2 text-sm lg:text-base rounded-md hover:bg-indigo-700 shadow-md w-2/5 sm:w-2/3'>Comprar ahora</button>
 
 
                     </div>
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,9 +2,14 @@ import React from 'react';
 import DetailCard from '../components/DetailCard';
 
 const Details = () => {
-    
+
     return (
-        <>
+        <div style={{
+            // backgroundColor: '#f3ebde'
+            // backgroundColor: '#f3ebde6b'
+            // backgroundColor: '#FAF6E9'
+            backgroundColor: '#FFFDF6'
+        }}>
             {/* Header */}
             <div className='mb-10 flex justify-center items-center h-20 border border-red-400'>
                 <p className='uppercase font-bold'>Details</p>
@@ -27,7 +32,7 @@ const Details = () => {
                 <p className='uppercase font-bold'>Footer</p>
             </div>
 
-        </>
+        </div>
     )
 }
 
