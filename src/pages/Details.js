c34ee80907bb0a0a61b0da0c66233570ceabaaba
tomasAybar <aybar.t4@gmail.com>
2022-07-19 13:56:06 -0300

    creacion de card detail en proceso

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
+    return (
+        <>
+            <div className='flex justify-center items-center h-20 border border-red-500'>
+                <p className='uppercase font-bold'>Details</p>
+            </div>
+
+            {/* contenedor principal */}
+            <div>
+
+                <div className='container mx-auto flex justify-evenly items-center'>
+
+                    {/* card productos y descripcion */}
+                    <div className='flex justify-center items-center flex-col'>
+                        {/* card productos */}
+                        <div>
+                            <img 
+                                src='https://wave.fr/images/1916/08/nike-air-max-1-wmns-triple-white.jpg'
+                            />
+                            <img 
+                                src='https://wave.fr/images/1916/08/nike-air-max-1-wmns-triple-white.jpg'
+                            />
+                            <img 
+                                src='https://wave.fr/images/1916/08/nike-air-max-1-wmns-triple-white.jpg'
+                            />
+                            <img 
+                                src='https://wave.fr/images/1916/08/nike-air-max-1-wmns-triple-white.jpg'
+                            />
+                        </div>
+
+                        {/* Descripcion */}
+                        <div>
+
+                        </div>
+                    </div>
+
+                    {/* Nombre precio talle */}
+                    <div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className='flex justify-center items-center h-20 border border-red-500'>
+                <p className='uppercase font-bold'>comentarios</p>
+            </div>
+
+            <div className='flex justify-center items-center h-20 border border-red-500'>
+                <p className='uppercase font-bold'>Footer</p>
+            </div>
+
+        </>
+    )
 }
 
 export default Details
