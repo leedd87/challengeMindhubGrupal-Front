b2bb6e2649ad31b4a1f1c0d2a53e14b9e088df3b
tomasAybar <aybar.t4@gmail.com>
2022-07-19 18:33:27 -0300

    cambios en app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import '../src/styles/App.css';
-import Details from './pages/Details';
 
 
 function App() {
   
   return (
-    // <div className="App">
-    //   <h1>hola</h1>
-    // </div>
+    <div className="App">
+      <h1>hola</h1>
+    </div>
 
-    <Details />
   );
 }
 
