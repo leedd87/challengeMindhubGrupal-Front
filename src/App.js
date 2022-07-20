5b77b286061e9e30e33dd92ea2ba07214a609f7e
Alexis Yusti <alexisyusti18@gmail.com>
2022-07-20 16:13:57 -0300

    carrito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './styles/App.css';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Nav from './components/Nav';
+import ShopCart from './components/ShopCart';
+import Carrito from './components/Carrito2';
 
 function App() {
   
   return (
     <div className="App">
-
+      <Nav/>
+      <Routes>
+        <Route path='/login' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/shopcart' element={<ShopCart/>}/>
+        <Route path='/carrito' element={<Carrito/>}/>
+      </Routes>
     </div>
   );
 }
