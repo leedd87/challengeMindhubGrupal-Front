96ec1ce84922d4d87583f15de013fee07d969035
tomasAybar <aybar.t4@gmail.com>
2022-07-20 16:33:36 -0300

    routes en app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import '../src/styles/App.css'
-import Shoop from './pages/Shop';
-import CardsShop from './components/CardsShop'
-
-=======
 import './styles/App.css';
 import { Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
@@ -12,21 +6,10 @@ import SignUp from './components/SignUp';
 import Nav from './components/Nav';
 import ShopCart from './components/ShopCart';
 import Carrito from './components/Carrito2';
->>>>>>> 1ce739c11e829fa5e6fb1ff3624a5e855d85d8a3
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    // <div className="App">
-    //   <h1>Hola</h1>
-   
-    // </div>
-
-    <Shoop />
-    // <CardsShop />
-
-=======
     <div className="App">
       <Nav/>
       <Routes>
@@ -36,7 +19,6 @@ function App() {
         <Route path='/carrito' element={<Carrito/>}/>
       </Routes>
     </div>
->>>>>>> 1ce739c11e829fa5e6fb1ff3624a5e855d85d8a3
 
   );
 }
