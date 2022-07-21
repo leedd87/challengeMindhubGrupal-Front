669c176d80fc18eabaeb722da46ceefd496f386a
tomasAybar <aybar.t4@gmail.com>
2022-07-21 14:40:14 -0300

    se retocaron detalles en las cards de shop

--- src/components/CardsShop.jsx
+++ src/components/CardsShop.jsx
@@ -13,20 +13,21 @@ const CardsShop = ({ shoes }) => {
 		console.log(res.data.response);
 	};
 
+	
 	return (
 		<div id="container">
 
 			<div className="wrapper">
 				<div className="card">
 					<div className="front">
-						<h1>Adidas</h1>
+						<h1>{shoes.brand.name}</h1>
 						<p>{shoes.name}</p>
 						<h2 className="price">U$S {shoes.price}</h2>
 					</div>
 
 					<div className="right">
-						<h2>Adidas</h2>
-						<p>Runing start</p>
+						<h2>{shoes.brand.name}</h2>
+						<p>{shoes.name}</p>
 						<h2 className="price">U$S {shoes.price}</h2>
 						<ul>
 							<li>talles : 12 12</li>
@@ -41,52 +42,10 @@ const CardsShop = ({ shoes }) => {
 
 				</div>
 				<div className="img-wrapper">
-					<img src={shoes.image[0]} alt='' />
+					<img src={shoes.image[0]} alt={shoes.name} />
 				</div>
 			</div>
-			{/* <article id="card">
-				<header>
-					<img
-						id="nikeLogo"
-						src="https://wallpapercave.com/wp/CgjGnw0.jpg"
-						alt="logo"
-					/>
-					<span id="nike" classNameName="subhead">
-						puma
-					</span>
-				</header>
-
-				<div
-					style={{
-						backgroundImage: `url('${shoes.image[0]}')`,
-						backgroundSize: "cover",
-						backgroundPosition: "center",
-						backgroundRepeat: "no-repeat",
-						width: 300,
-						height: 200,
-						objectFit: "cover",
-					}}
-				></div>
-
-				<section>
-					<div id="location">
-						<span classNameName="faded-name">{shoes.name}</span>
-						<span classNameName="faded">U$S {shoes.price}</span>
-					</div>
-
-					<div classNameNameName="flex items-center justify-around mb-5">
-						<button classNameNameName="bg-purple-600 text-white font-bold rounded-md hover:bg-purple-700 px-7 py-2">
-							buy
-						</button>
-						<button
-							onClick={() => handleIDshoe(shoes._id)}
-							classNameNameName="bg-purple-600 text-white font-bold rounded-md hover:bg-purple-700 px-7 py-2"
-						>
-							+ info
-						</button>
-					</div>
-				</section>
-			</article> */}
+			
 		</div>
 	);
 };
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -8,7 +8,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-/*optionsFilter*/ 
+/*optionsFilter*/
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -19,102 +19,104 @@ import Select from '@mui/material/Select';
 
 
 function Shoop() {
-  const [age, setAge] = React.useState('');
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  }
-  const dispatch = useDispatch()
+    const [age, setAge] = React.useState('');
 
-  useEffect(() => {
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    }
 
-    dispatch(shoesActions.getShoes())
+    const dispatch = useDispatch()
 
-    // eslint-disable-next-line
-  }, [])
+    useEffect(() => {
 
-  const shoes = useSelector(store => store.shoesReducer.shoes);
+        dispatch(shoesActions.getShoes())
 
-  return (
+        // eslint-disable-next-line
+    }, [])
 
-    <>
-      <div className='Header mb-10 bg-gray-300 py-5'>
+    const shoes = useSelector(store => store.shoesReducer.shoes);
 
-        <Accordion>
-          <AccordionSummary
-            // expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
+    return (
 
-          >
-            <Typography >Filter</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            Gender:
-            <select name="gender" id="">
+        <>
+            <div className='Header mb-10 bg-gray-300 py-5'>
 
-            </select>
+                <Accordion>
+                    <AccordionSummary
+                        // expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
 
-          </AccordionDetails>
-        </Accordion>
-      </div>
+                    >
+                        <Typography >Filter</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        Gender:
+                        <select name="gender" id="">
 
-      {/* CONTENEDOR DE CARDS */}
-      <div className='contFilterCards'>
-        <div className='filter'>
-          <div>
-          <h1>Filter Shoes</h1>
-          <h3>Price</h3>
-          <Box sx={{ minWidth: 120, padding:1}}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-          <h3>Color:</h3>
-          <div className='itemsFilterColor'>
-          
-            <p>⚫⚪🟤</p>
-            <p>🔴🔵🟠</p>
-            <p>🟡🟢</p>
-           
-          </div>
-          </div>
-         
-        </div>
+                        </select>
 
-        <div className='bodyShop mb-10'>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
 
-          {
-            shoes?.map((shoes, index) => {
+            {/* CONTENEDOR DE CARDS */}
+            <div className='contFilterCards'>
+                <div className='filter'>
+                    <div>
+                        <h1>Filter Shoes</h1>
+                        <h3>Price</h3>
+                        <Box sx={{ minWidth: 120, padding: 1 }}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={age}
+                                    label="Age"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
+                        <h3>Color:</h3>
+                        <div className='itemsFilterColor'>
 
-              return (
+                            <p>⚫⚪🟤</p>
+                            <p>🔴🔵🟠</p>
+                            <p>🟡🟢</p>
 
-                <CardsShop key={index} shoes={shoes} />
+                        </div>
+                    </div>
 
-              )
+                </div>
 
-            })
-          }
+                <div className='bodyShop mb-10'>
 
-        </div>
-      </div>
-      <div className='bg-red-400 h-20 flex items-center justify-center'>
-        <h2 className=''>Footer</h2>
-      </div>
-    </>
+                    {
+                        shoes?.map((shoes, index) => {
 
-  )
+                            return (
+
+                                <CardsShop key={index} shoes={shoes} />
+
+                            )
+
+                        })
+                    }
+
+                </div>
+            </div>
+            <div className='bg-red-400 h-20 flex items-center justify-center'>
+                <h2 className=''>Footer</h2>
+            </div>
+        </>
+
+    )
 }
 
 export default Shoop;
