38791dcc03d2355322c75587f42d71a44d2e29ae
tomasAybar <aybar.t4@gmail.com>
2022-07-21 18:54:18 -0300

    creacion de action y reducer shop

--- src/redux/reducers/mainReducers.js
+++ src/redux/reducers/mainReducers.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux'
-import shoesReducer from '../../../src/redux/reducers/shoesReducer'
+import shoesReducer from '../../../src/redux/reducers/shoesReducer';
+import shopReducer from '../reducers/shopReducers';
 
 const mainReducer=combineReducers({
-    shoesReducer
+    shoesReducer,
+    shopReducer
 })
 export default mainReducer
