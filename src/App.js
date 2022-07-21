9a6ee4a5cf5872d0beceb2c79b58fd4dea518255
Alexis Yusti <alexisyusti18@gmail.com>
2022-07-21 18:52:23 -0300

    rutas

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import './styles/App.css';
 import { Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-import Nav from './components/Nav';
+import NavBar from './components/Navbar'
+import Footer from './components/Footer'
 import Account from './components/Account';
 import AdminForm from './components/AdminForm';
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import Details from './pages/Details'
 
 function App() {
   
   return (
     <div className="App">
-      <Nav/>
+      <NavBar/>
       <Routes>
         <Route path='/signIn' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/account' element={<Account/>}/>
         <Route path='/adminForm' element={<AdminForm/>}/>
+		<Route path='/shop' element={<Shop/>}/>
+		<Route path='/' element={<Home/>}/>
+		<Route path='/details/:id' element={<Details/>}/>
       </Routes>
+	  <Footer/>
     </div>
   );
 }
