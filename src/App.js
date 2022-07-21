aaf9aeee57607b2496cdd4bb3f3644e154a30d3d
FlorenciaRomano <florromanoyt@gmail.com>
2022-07-21 14:52:44 -0300

    flor

--- src/App.js
+++ src/App.js
@@ -1,61 +1,26 @@
 import React from 'react'
-import '../src/styles/App.css'
-
+import './styles/App.css';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Nav from './components/Nav';
+import ShopCart from './components/ShopCart';
+import Carrito from './components/Carrito2';
 
 function App() {
 
   return (
     <div className="App">
-        {/* <div className='Header'>
-        <h2>NavBar</h2>
-        <h1>Shop</h1>
-
-        <div className='botonesShop'>
-        <button>Deplegable1</button>
-        <button>Desplegable2</button>
-        <button>Desplegable3</button>
-        <button>Desplegable4</button>
-        </div>
-      </div>
-
-      <div className='bodyShop'>
-      <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvd9KD93IRVly4R5GBjLr7ViHk0NG6eTBtzQ&usqp=CAU'/>
-        </div>
-      </div>
-
-      <div className='Footer'>
-       <h2>Footer</h2>
-      </div> */}
-   
+      <Nav/>
+      <Routes>
+        <Route path='/login' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/shopcart' element={<ShopCart/>}/>
+        <Route path='/carrito' element={<Carrito/>}/>
+      </Routes>
     </div>
 
   );
 }
+
 export default App;
