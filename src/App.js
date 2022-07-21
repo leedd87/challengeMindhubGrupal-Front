6520e45534aa4541f2c93f375968b112ac189a84
tomasAybar <aybar.t4@gmail.com>
2022-07-21 09:57:28 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react'
-import './styles/App.css';
-import { Route, Routes } from 'react-router-dom';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import Nav from './components/Nav';
-import ShopCart from './components/ShopCart';
-import Carrito from './components/Carrito2';
+import '../src/styles/App.css'
+import Shoop from './pages/Shop';
+import CardsShop from './components/CardsShop'
+
 
 function App() {
 
   return (
-    <div className="App">
-      <Nav/>
-      <Routes>
-        <Route path='/login' element={<SignIn/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/shopcart' element={<ShopCart/>}/>
-        <Route path='/carrito' element={<Carrito/>}/>
-      </Routes>
-    </div>
+    // <div className="App">
+    //   <h1>Hola</h1>
+   
+    // </div>
+
+    <Shoop />
+    // <CardsShop />
+
 
   );
 }
-
 export default App;
