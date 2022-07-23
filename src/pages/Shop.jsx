8b3b8f1d1aa8d0c24d95fc0fe9e16c59176edaf5
Laura Silva <lauradsilvah@gmail.com>
2022-07-23 14:37:53 -0300

    filtros por precio

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -15,38 +15,68 @@ import Select from '@mui/material/Select';
 function Shoop() {
   const dispatch = useDispatch()
   const [inputSearch, setInputSearch] = useState("")
-  const [tipe, setTipe] = React.useState('');
+  const [type, setType] = React.useState('');
   const [brand, setBrand] = React.useState('');
   const [shoes, setShoes] = React.useState([]);
+
+ const [reload, setReload]=React.useState(false);
   const [filterShoes, setfilterShoes] = React.useState([]);
 
   console.log("🚀 ~ file: Shop.jsx ~ line 22 ~ Shoop ~ filterShoes", filterShoes)
   useEffect(() => {
     //dispatch(shoesActions.filterShoes(inputSearch))
     getShoes()
+  
     // eslint-disable-next-line
   }, [])
   let filterCard = useSelector(store => store.shoesReducer.filterShoes)
 
-  useEffect(()=>{
+  useEffect(() => {
     if (shoes?.length > 0) {
-      const filterRender = shoes?.filter(shoe => shoe.name.toLowerCase().startsWith(inputSearch.trim().toLowerCase()))
+      const filterRender = shoes?.filter(shoe =>
+        shoe.name.toLowerCase().startsWith(inputSearch.trim().toLowerCase())
+        || shoe.brand.name.toLowerCase().startsWith(inputSearch.trim().toLowerCase())
+        // || shoe.type[0].name.toLowerCase().startsWith(inputSearch.trim().toLowerCase())
+      )
       setfilterShoes(filterRender)
     }
     // eslint-disable-next-line
-  },[inputSearch])
+  }, [inputSearch])
 
-  
+
   const getShoes = async (event) => {
     const res = await dispatch(shoesActions.getShoes())
     console.log("🚀 ~ file: Shop.jsx ~ line 53 ~ handleChangeBrand ~ res", res)
     setShoes(res.data.response)
-   
+    setfilterShoes(res.data.response)
+    setInputSearch("")
+    setBrand("")
   }
 
-  const handleChangeTipe = async (event) => {
-    console.log(event.target.value);
-    const res = await dispatch(shoesActions.getShoesByType(event.target.value))
+  const order = () =>{
+    //setShoes("")
+    shoes?.sort((a, b) =>  a.price - b.price);
+    setReload(!reload)
+    //setShoes("")
+    //setShoes(listaOrd)
+    //console.log(listaOrd);
+    console.log(shoes)
+  }
+  const order2 = () =>{
+    //setShoes("")
+    shoes?.sort((a, b) =>  b.price - a.price);
+    setReload(!reload)
+    //setShoes("")
+    //setShoes(listaOrd)
+    //console.log(listaOrd);
+    console.log(shoes)
+  }
+
+
+
+  const handleChangeType = async (id) => {
+    console.log(id);
+    const res = await dispatch(shoesActions.getShoesByType(id))
     setShoes(res.response.response)
     setfilterShoes(res.response.response)
   }
@@ -56,50 +86,45 @@ function Shoop() {
     console.log("🚀 ~ file: Shop.jsx ~ line 53 ~ handleChangeBrand ~ res", res)
     setShoes(res.response.response)
     setfilterShoes(res.response.response)
+    setBrand(event.target.value)
   }
   //   const search=(e)=>{
   //     dispatch(shoesActions.filterShoes(e.target.value))
   // }
-  
+
 
   return (
     <>
-      <div className='Header mb-9 bg-gray-300 py-5'>
+      <div className='Header bg-gray-300 py-5'>
 
         <div className='filtersShop'>
-
-          <div>
-            <input className='input' type="text" placeholder='Search Shoes' onChange={(e) => setInputSearch(e.target.value)} />
-          </div>
-
-          <Box sx={{ minWidth: 120, padding: 1 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Tipe</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={tipe}
-                label="Tipe"
-                onChange={handleChangeTipe}
-              >
-                <MenuItem value={"62d81e84db36588e63203de7"}>Urban</MenuItem>
-                <MenuItem value={"62d827534fca1cb3de1808b1"}>Sport</MenuItem>
-                <MenuItem value={"62d828964fca1cb3de1808bd"}>Fancy</MenuItem>
-              </Select>
-            </FormControl>
+          <Box>
+            <button className='btnColors' onClick={() => handleChangeType("62d81e84db36588e63203de7")}>Urban</button>
+          </Box>
+          <Box>
+            <button className='btnColors' onClick={() => handleChangeType("62d827534fca1cb3de1808b1")}>Sport</button>
+          </Box>
+          <Box>
+            <button className='btnColors' onClick={() => handleChangeType("62d828964fca1cb3de1808bd")}>Fancy</button>
           </Box>
 
+          <div>
+            <input value={inputSearch} className='input' type="text" placeholder='Search Shoes' onChange={(e) => setInputSearch(e.target.value)} />
+          </div>
 
-          <Box sx={{ minWidth: 120, padding: 1 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Brand</InputLabel>
+          <Box>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="demo-simple-select-standard-label">Brand</InputLabel>
               <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
                 value={brand}
-                label="Brand"
                 onChange={handleChangeBrand}
+                label="Brand"
               >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
                 <MenuItem value={"62d836532f6e1f16ca3df337"}>Nike</MenuItem>
                 <MenuItem value={"62d838004fca1cb3de1808cb"}>Adidas</MenuItem>
                 <MenuItem value={"62d8392a4fca1cb3de1808cd"}>Jordan</MenuItem>
@@ -108,15 +133,23 @@ function Shoop() {
                 <MenuItem value={"62d83b334fca1cb3de1808d1"}>Balenciaga</MenuItem>
                 <MenuItem value={"62d83c814fca1cb3de1808d6"}>Dior</MenuItem>
                 <MenuItem value={"62d83e2d4fca1cb3de1808da"}>Louis Vuittom</MenuItem>
-
               </Select>
             </FormControl>
           </Box>
 
+        
+          <Box>
+            <button className='btnColors' onClick={ order} >por precio</button>
+          </Box>
+          <Box>
+            <button className='btnColors' onClick={ order2} >por precio M-me</button>
+          </Box>
+
+          <Box>
+            <button className='btnColors' onClick={() => getShoes()} >clear</button>
+          </Box>
+
         </div>
-
-
-
 
       </div>
 
@@ -150,12 +183,14 @@ function Shoop() {
 
                 })
               ) :
-              shoes.map((shoes, index) => {
+              (shoes.map((shoes, index) => {
                 return (
                   <CardsShop key={index} shoes={shoes} />
                 )
 
-              })
+              })) ?
+                (<h1>holaaa</h1>) : null
+
           }
 
         </div>
