880b3767fdf11d4e86923f1e2fe1a9d5a1156f07
tomasAybar <aybar.t4@gmail.com>
2022-07-24 00:16:58 -0300

    alertas en consola funcionando

--- src/components/AdminForm.jsx
+++ src/components/AdminForm.jsx
@@ -1,16 +1,41 @@
 import '../../src/styles/style.css'
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { useState } from 'react';
+// import Box from '@mui/material/Box';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+// import Select from '@mui/material/Select';
+import { useState, useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import adminActions from '../redux/actions/adminActions';
+import shoesActions from '../redux/actions/shoesActions';
 
 
 export default function AdminForm() {
 
+    const [shoes, setShoes] = useState([])
+    const [reload, setReload] = useState(false)
+    const dispatch = useDispatch()
 
-    const handleSubmit = (e) => {
+    useEffect(() => {
+
+        // getShoes()
+        dispatch(shoesActions.getShoes())
+            .then(res => setShoes(res.data.response))
+
+    }, [reload])
+
+    // const getShoes = async () => {
+
+    //     let shoes = await dispatch(shoesActions.getShoes())
+
+    //     setShoes(shoes.data.response)
+
+    // }
+
+    // AGREGAR SHOE
+    const handleSubmit = async (e) => {
         e.preventDefault()
         // console.log(e.target)
 
@@ -25,8 +50,8 @@ export default function AdminForm() {
             type: e.target[10].value,
         }
 
-        console.log(data)
-
+        const res = await dispatch(adminActions.addShoe(data))
+        console.log(res.data.message)
 
         e.target[0].value = ''
         e.target[1].value = ''
@@ -39,80 +64,92 @@ export default function AdminForm() {
         e.target[8].value = ''
         e.target[9].value = ''
         e.target[10].value = ''
+
+
+        setReload(!reload)
+        // getShoes()
+    }
+
+
+    // REMOVE SHOE
+    const handleSubmitDelete = async (e) => {
+        e.preventDefault()
+
+        const res = await dispatch(adminActions.removeShoe(e.target[0].value))
+        console.log(res.data.message)
+        e.target[0].value = ''
+
+        setReload(!reload)
     }
 
     return (
         <div className="adminForm">
 
-            <div className='admin-ctn'>
-                <div className='title-form'>Add Shoes</div>
-                <form
-                    className='form-admin'
-                    onSubmit={handleSubmit}
-                >
-                    <div className='admin-details'>
-                        <div className='input-box'>
-                            <input className='input-admin' type='text' placeholder='Name' required />
-                        </div>
+            <div className='grid grid-cols-2'>
 
-                        {/* BRAND */}
-                        <select name="type" id="" required className='p-2 w-4/5 rounded-md'>
-                            <option value=""></option>
-                            <option value="62d836532f6e1f16ca3df337">Nike</option>
-                            <option value="62d838004fca1cb3de1808cb">Adidas</option>
-                            <option value="62d8392a4fca1cb3de1808cd">Jordan</option>
-                            <option value="62d839d04fca1cb3de1808ce">New Balance</option>
-                            <option value="62d83a704fca1cb3de1808cf">Asics</option>
-                            <option value="62d83b334fca1cb3de1808d1">Balenciaga</option>
-                            <option value="62d83c814fca1cb3de1808d6">Dior</option>
-                            <option value="62d83e2d4fca1cb3de1808da">Louis Vuittom</option>
-                        </select>
-                        {/* BRAND */}
-                        {/* <div className='input-box'>
-                            <input className='input-admin' type='text' placeholder='Brand' required />
-                        </div> */}
+                {/* ADD SHOE */}
+                <div>
+                    <div className='title-form'>Add Shoes</div>
+                    <form
+                        className='form-admin'
+                        onSubmit={handleSubmit}
+                    >
+                        <div className='admin-details'>
+                            <div className='input-box'>
+                                <input className='input-admin' type='text' placeholder='Name' required />
+                            </div>
 
+                            {/* BRAND */}
+                            <select name="type" id="" required className='p-2 w-4/5 rounded-md'>
+                                <option value=""></option>
+                                <option value="62d836532f6e1f16ca3df337">Nike</option>
+                                <option value="62d838004fca1cb3de1808cb">Adidas</option>
+                                <option value="62d8392a4fca1cb3de1808cd">Jordan</option>
+                                <option value="62d839d04fca1cb3de1808ce">New Balance</option>
+                                <option value="62d83a704fca1cb3de1808cf">Asics</option>
+                                <option value="62d83b334fca1cb3de1808d1">Balenciaga</option>
+                                <option value="62d83c814fca1cb3de1808d6">Dior</option>
+                                <option value="62d83e2d4fca1cb3de1808da">Louis Vuittom</option>
+                            </select>
 
+                            <div className='input-box'>
+                                <input className='input-admin' type='text' placeholder='Colorway' required />
+                            </div>
+                            <div className='input-box'>
+                                <input className='input-admin' type='number' placeholder='Price' required />
+                            </div>
 
-                        <div className='input-box'>
-                            <input className='input-admin' type='text' placeholder='Colorway' required />
-                        </div>
-                        <div className='input-box'>
-                            <input className='input-admin' type='number' placeholder='Price' required />
-                        </div>
-
-
-                        {/* IMAGENES */}
-                        <div className='input-box'>
-                            <input className='input-admin' type='text' placeholder='url img' required />
-                        </div>
-                        <div className='input-box'>
-                            <input className='input-admin' type='text' placeholder='url img' required />
-                        </div>
-                        <div className='input-box'>
-                            <input className='input-admin' type='text' placeholder='url img' required />
-                        </div>
-                        <div className='input-box'>
-                            <input className='input-admin' type='text' placeholder='url img' />
-                        </div>
-                        {/* IMAGENES */}
+                            {/* IMAGENES */}
+                            <div className='input-box'>
+                                <input className='input-admin' type='text' placeholder='url img' required />
+                            </div>
+                            <div className='input-box'>
+                                <input className='input-admin' type='text' placeholder='url img' required />
+                            </div>
+                            <div className='input-box'>
+                                <input className='input-admin' type='text' placeholder='url img' required />
+                            </div>
+                            <div className='input-box'>
+                                <input className='input-admin' type='text' placeholder='url img' />
+                            </div>
+                            {/* IMAGENES */}
 
 
-                        <div className='input-box'>
-                            <input className='input-admin' type='text' placeholder='Description' required />
-                        </div>
-                        <div className='input-box'>
-                            <input className='input-admin' type='number' placeholder='Stock' required />
-                        </div>
+                            <div className='input-box'>
+                                <input className='input-admin' type='text' placeholder='Description' required />
+                            </div>
+                            <div className='input-box'>
+                                <input className='input-admin' type='number' placeholder='Stock' required />
+                            </div>
 
-                        <select name="type" id="" required className='p-2 w-4/5 rounded-md'>
-                            <option value=""></option>
-                            <option value="62d81e84db36588e63203de7">urban</option>
-                            <option value="62d827534fca1cb3de1808b1">sport</option>
-                            <option value="62d828964fca1cb3de1808bd">fancy</option>
-                        </select>
+                            <select name="type" id="" required className='p-2 w-4/5 rounded-md'>
+                                <option value=""></option>
+                                <option value="62d81e84db36588e63203de7">urban</option>
+                                <option value="62d827534fca1cb3de1808b1">sport</option>
+                                <option value="62d828964fca1cb3de1808bd">fancy</option>
+                            </select>
 
-                        {/* <InputLabel id="demo-simple-select-standard-label">Type</InputLabel>
+                            {/* <InputLabel id="demo-simple-select-standard-label">Type</InputLabel>
                         <Select
                             labelId="demo-simple-select-standard-label"
                             id="demo-simple-select-standard"
@@ -127,14 +164,41 @@ export default function AdminForm() {
                         </Select> */}
 
 
-                        {/* <div className='input-box'>
+                            {/* <div className='input-box'>
                             <input className='input-admin' type='text' placeholder='Type'/>
                         </div> */}
-                    </div>
-                    <div className='button-ctn'>
-                        <input className='btn-admin' type='submit' value='send' />
-                    </div>
-                </form>
+                        </div>
+                        <div className='button-ctn'>
+                            <input className='btn-admin' type='submit' value='send' />
+                        </div>
+                    </form>
+                </div>
+
+                {/* DELETE SHOE */}
+                <div>
+                    <div className='title-form'>Remove Shoes</div>
+
+                    <form
+                        className='form-admin'
+                        onSubmit={handleSubmitDelete}
+                    >
+                        {/* <div className='admin-details'> */}
+
+                        <select name="type" id="" required className='p-2 w-4/5 rounded-md'>
+
+                            {shoes?.map(shoe => <option key={shoe._id} value={shoe._id}>{shoe.name}</option>)}
+
+                        </select>
+
+                        {/* </div> */}
+
+                        <div className='button-ctn'>
+                            <input className='btn-admin' type='submit' value='send' />
+                        </div>
+
+                    </form>
+                </div>
+
             </div>
         </div>
     )
