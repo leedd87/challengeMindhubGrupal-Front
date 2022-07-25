15bc9ffdebb1ae6051414e8d8e79b4f70584ea39
FlorenciaRomano <florromanoyt@gmail.com>
2022-07-25 11:55:20 -0300

    flor

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,167 +1,194 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import shoesActions from '../redux/actions/shoesActions'
+import { useDispatch} from 'react-redux';
+import { useState } from 'react';
+import shoesActions from '../redux/actions/shoesActions';
 import CardsShop from '../components/CardsShop';
 import '../styles/style.css'
-/*acordeon*/
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
 /*optionsFilter*/
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
-import SearchAppBar from '../components/inputShop'
-
-
-
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
 function Shoop() {
-  const [age, setAge] = React.useState('');
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  }
   const dispatch = useDispatch()
+  const [inputSearch, setInputSearch] = useState("")
+  const [brand, setBrand] = React.useState('');
+  const [shoes, setShoes] = React.useState([]);
+  const [ord, setOrd] = React.useState('');
+  const [reload, setReload] = React.useState(false);
+  const [filterShoes, setfilterShoes] = React.useState([]);
 
+  console.log("🚀 ~ file: Shop.jsx ~ line 22 ~ Shoop ~ filterShoes", filterShoes)
   useEffect(() => {
-
-    dispatch(shoesActions.getShoes())
+    //dispatch(shoesActions.filterShoes(inputSearch))
+    getShoes()
 
     // eslint-disable-next-line
   }, [])
 
-  const shoes = useSelector(store => store.shoesReducer.shoes);
+
+  useEffect(() => {
+    if (shoes?.length > 0) {
+      const filterRender = shoes?.filter(shoe =>
+        shoe.name.toLowerCase().startsWith(inputSearch.trim().toLowerCase())
+        || shoe.brand.name.toLowerCase().startsWith(inputSearch.trim().toLowerCase())
+        // || shoe.type[0].name.toLowerCase().startsWith(inputSearch.trim().toLowerCase())
+      )
+      setfilterShoes(filterRender)
+    }
+    // eslint-disable-next-line
+  }, [inputSearch])
+
+
+  const getShoes = async (event) => {
+    const res = await dispatch(shoesActions.getShoes())
+    console.log("🚀 ~ file: Shop.jsx ~ line 53 ~ handleChangeBrand ~ res", res)
+    setShoes(res.data.response)
+    setfilterShoes(res.data.response)
+    setInputSearch("")
+    setBrand("")
+    setOrd("")
+  }
+
+  const order = (event) => {
+    console.log(event)
+    if (event.target.value === "op1") {
+      shoes?.sort((a, b) => a.price - b.price);
+      setOrd(event.target.value)
+      setReload(!reload)
+    }
+    else if (event.target.value === "op2"){
+      shoes?.sort((a, b) => b.price - a.price)
+      setReload(!reload)
+      setOrd(event.target.value)
+    }
+    
+  }
+
+
+  const handleChangeType = async (id) => {
+    console.log(id);
+    const res = await dispatch(shoesActions.getShoesByType(id))
+    setShoes(res.response.response)
+    setfilterShoes(res.response.response)
+  }
+
+  const handleChangeBrand = async (event) => {
+    const res = await dispatch(shoesActions.getShoesByBrand(event.target.value))
+    console.log("🚀 ~ file: Shop.jsx ~ line 53 ~ handleChangeBrand ~ res", res)
+    setShoes(res.response.response)
+    setfilterShoes(res.response.response)
+    setBrand(event.target.value)
+  }
+  //   const search=(e)=>{
+  //     dispatch(shoesActions.filterShoes(e.target.value))
+  // }
+
 
   return (
-
     <>
-      <div className='Header mb-9 bg-gray-300 py-5'>
+      <div className='Header bg-gray-300 py-5'>
 
-       
-          <AccordionSummary
-            // expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
+        <div className='filtersShop'>
+          <Box>
+            <button className='btnColors' onClick={() => handleChangeType("62d81e84db36588e63203de7")}>Urban</button>
+          </Box>
+          <Box>
+            <button className='btnColors' onClick={() => handleChangeType("62d827534fca1cb3de1808b1")}>Sport</button>
+          </Box>
+          <Box>
+            <button className='btnColors' onClick={() => handleChangeType("62d828964fca1cb3de1808bd")}>Fancy</button>
+          </Box>
 
-          > <div className='filtersShop'>
-            <div className='filterGender'>
-            <AccordionDetails>
-              Gender:
+          <div>
+            <input value={inputSearch} className='input' type="text" placeholder='Search Shoes' onChange={(e) => setInputSearch(e.target.value)} />
+          </div>
+
+          <Box>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="demo-simple-select-standard-label">Brand</InputLabel>
               <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={age}
-                label="Age"
-                onChange={handleChange}
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={brand}
+                onChange={handleChangeBrand}
+                label="Brand"
               >
-                <MenuItem value={0}>Man</MenuItem>
-                <MenuItem value={1}>Woman</MenuItem>
-                <MenuItem value={2}>Kids</MenuItem>
-                <MenuItem value={3}>Unisex</MenuItem>
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={"62d836532f6e1f16ca3df337"}>Nike</MenuItem>
+                <MenuItem value={"62d838004fca1cb3de1808cb"}>Adidas</MenuItem>
+                <MenuItem value={"62d8392a4fca1cb3de1808cd"}>Jordan</MenuItem>
+                <MenuItem value={"62d839d04fca1cb3de1808ce"}>New Balance</MenuItem>
+                <MenuItem value={"62d83a704fca1cb3de1808cf"}>Asics</MenuItem>
+                <MenuItem value={"62d83b334fca1cb3de1808d1"}>Balenciaga</MenuItem>
+                <MenuItem value={"62d83c814fca1cb3de1808d6"}>Dior</MenuItem>
+                <MenuItem value={"62d83e2d4fca1cb3de1808da"}>Louis Vuittom</MenuItem>
               </Select>
-              </AccordionDetails>
-              </div>
+            </FormControl>
+          </Box>
 
-              <div className='filterMark'>
-              <AccordionDetails>
-                Marks:
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={age}
-                  label="Age"
-                  onChange={handleChange}
-                >
-                  <MenuItem value={0}>Nike</MenuItem>
-                  <MenuItem value={1}>Adidas</MenuItem>
-                  <MenuItem value={2}>New Balance</MenuItem>
-                  <MenuItem value={3}>Jordan</MenuItem>
-                  <MenuItem value={4}>Asics</MenuItem>
-                  <MenuItem value={5}>Balenciaga</MenuItem>
-                  <MenuItem value={6}>Dior</MenuItem>
-                  <MenuItem value={7}>Louis Vuitton</MenuItem>
+          <Box>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="demo-simple-select-standard-label">Price</InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={ord}
+                onChange={order}
+                label="Brand"
+              >
+                <MenuItem value={"op1"}>menor a mayor</MenuItem>
+                <MenuItem value={"op2"}>mayor a menor</MenuItem>
+                
+              </Select>
+            </FormControl>
+          </Box>
 
+          <Box>
+            <button className='btnColors' onClick={() => getShoes()} ><DeleteIcon/></button>
+          </Box>
 
-
-                </Select>
-
-
-              </AccordionDetails>
-              </div>
-              <SearchAppBar/>
-              </div>
-
-          </AccordionSummary>
-
-      
+        </div>
 
       </div>
 
       {/* CONTENEDOR DE CARDS */}
       <div className='contFilterCards'>
-        <div className='filter'>
-          <div>
-            <h1>Filter Shoes</h1>
-            <h3>Price</h3>
-            <Box sx={{ minWidth: 120, padding: 1 }}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={age}
-                  label="Age"
-                  onChange={handleChange}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-            <h3>Color:</h3>
-            <div className='itemsFilterColor'>
-
-              <p>⚫⚪🟤</p>
-              <p>🔴🔵🟠</p>
-              <p>🟡🟢</p>
-
-            </div>
-          </div>
-
-        </div>
 
         <div className='bodyShop mb-10'>
 
           {
-            shoes?.map((shoes, index) => {
+            filterShoes.length > 0 ?
+              (
+                filterShoes?.map((shoes, index) => {
+                  return (
+                    <CardsShop key={index} shoes={shoes} />
+                  )
 
-              return (
+                })
+              ) :
+              (shoes.map((shoes, index) => {
+                return (
+                  <CardsShop key={index} shoes={shoes} />
+                )
 
-                <CardsShop key={index} shoes={shoes} />
+              })) ?
+                (<h1>holaaa</h1>) : null
 
-              )
-
-            })
           }
 
         </div>
       </div>
-      <div className='bg-red-400 h-20 flex items-center justify-center'>
-        <h2 className=''>Footer</h2>
-      </div>
+
     </>
 
   )
 }
 
 export default Shoop;
-
-
